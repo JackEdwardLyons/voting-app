c5a503af125b96a35a7b0844884bebd861b6b41a
JackEdwardLyons <jack.edward.lyons@gmail.com>
2017-03-14 20:39:18 +1000

    Add notes on HTMLInputElement types, which bind to the input element in the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  /* NOTES on type syntax: HTMLInpurElement
+   * =======================================
+   * #newtitle and #newLink are now Objects that represent their associated input DOM elements 
+   * (specifically, the type is HTMLInputElement). Because they are both Objects now, that means 
+   * we get the value of the input tag using the '.value' method.
+   * */
 
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Article title: ${title.value} and link: ${link.value}`);
